e2f499b13e707d0c0dfa9a9d5d844989fd2a5ff3
Kenji <designer.kenji@gmail.com>
2016-05-06 00:22:42 -0400

    get WEB_CLIENT_ID

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ import protorpc
 
 from models import Student, Assignment, GradeEntry
 
-WEB_CLIENT_ID=""
+WEB_CLIENT_ID="59444852514-aj0l07tn4arg7c8a2q75up3np75mg5a3.apps.googleusercontent.com"
 ANDROID_CLIENT_ID=""
 IOS_CLIENT_ID=""
 
