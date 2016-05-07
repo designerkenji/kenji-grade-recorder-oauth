ca13f7e1c4cf1d5202edb3f1771b60adb24d4b8c
Kenji <designer.kenji@gmail.com>
2016-05-07 01:11:47 -0400

    debug ndb to EndpointModel

--- api.py
+++ api.py
@@ -10,57 +10,42 @@ import main
 from models import Student, Assignment, GradeEntry
 
 
-WEB_CLIENT_ID="59444852514-aj0l07tn4arg7c8a2q75up3np75mg5a3.apps.googleusercontent.com"
+WEB_CLIENT_ID=""
 ANDROID_CLIENT_ID=""
 IOS_CLIENT_ID=""
 
-@endpoints.api(name="graderecorder", 
-               version="v1", 
-               description="Grade Recorder API", 
-               audiences=[WEB_CLIENT_ID],
-               hostname="kenji-grade-recorder-oathu.appspot.com",
-               allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID, WEB_CLIENT_ID,ANDROID_CLIENT_ID,IOS_CLIENT_ID])
+@endpoints.api(name="graderecorder", version="v1", description="Grade Recorder API", hostname="boutell-grade-recorder.appspot.com", 
+               audiences=[WEB_CLIENT_ID], 
+               allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID, WEB_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID])
 class GradeRecorderApi(protorpc.remote.Service):
     pass
 
-    @Student.query_method(user_required="True", 
-                          query_fields=("limit", "pageToken"),
-                          name="student.list", 
-                          path="student/list", 
-                          http_method="GET")
+    @Student.query_method(user_required=True, query_fields=("limit", "pageToken"), 
+                          name="student.list", path="student/list", http_method="GET")
     def student_list(self, query):
-        """ list all the students for a given user """
+        """ List all the students for a given user """
         user = endpoints.get_current_user()
         students = Student.query(ancestor=main.get_parent_key(user)).order(Student.rose_username)
         return students
 
-    @Assignment.query_method(user_required="True", 
-                             query_fields=("limit","pageToken"),
-                             name="assignment.list", 
-                             path="assignment/list", 
-                             http_method="GET")
+    @Assignment.query_method(user_required=True, query_fields=("limit", "pageToken"), 
+                          name="assignment.list", path="assignment/list", http_method="GET")
     def assignment_list(self, query):
-        """ list all the assignments owned by a given user """
+        """ List all the assignments owned by the given user """
         user = endpoints.get_current_user()
         assignments = Assignment.query(ancestor=main.get_parent_key(user)).order(Assignment.name)
         return assignments
 
-    @GradeEntry.query_method(user_required="True", 
-                             query_fields=("limit","order","pageToken","assignment_key"),
-                             name="gradeentry.list", 
-                             path="gradeentry/list/{assignment_key}", 
-                             http_method="GET")
+    @GradeEntry.query_method(user_required = True, query_fields=("limit", "order", "pageToken", "assignment_key"), 
+                             name="gradeentry.list", path="gradeentry/list/{assignment_key}", http_method="GET")
     def gradeentry_list(self, query):
-        """ list all the grade entries for a given assignment """
+        """ List all the grade entries for a given assignment """
         return query
-    
-    @Assignment.method(user_required="True", 
-                       name="assignment.insert", 
-                       path="assignment/insert", 
-                       http_method="POST")
+
+    @Assignment.method(user_required=True, name="assignment.insert", path="assignment/insert", http_method = "POST")
     def assignment_insert(self, assignment):
-        """ add or update an assignment owned by the given user """
-        if assignment.form_datastore:
+        """ Add or update an assignment owned by the given user """
+        if assignment.from_datastore:
             assignment_with_parent = assignment
         else:
             user = endpoints.get_current_user()
@@ -68,31 +53,26 @@ class GradeRecorderApi(protorpc.remote.Service):
         assignment_with_parent.put()
         return assignment_with_parent
 
-    @GradeEntry.method(user_required="True", 
-                       name="gradeentry.insert", 
-                       path="gradeentry/insert", 
-                       http_method="POST")
+    @GradeEntry.method(user_required=True, name="gradeentry.insert", path="gradeentry/insert", http_method = "POST")
     def gradeentry_insert(self, grade_entry):
-        """ add or update an grade entry for an assignment """
-        if grade_entry.form_datastore:
+        """ Add or update a grade entry for an assignment """
+        if grade_entry.from_datastore:
             grade_entry_with_parent = grade_entry
         else:
             student = grade_entry.student_key.get()
-            grade_entry_with_parent = GradeEntry(parent=grade_entry.assignment_key,
+            grade_entry_with_parent = GradeEntry(parent=grade_entry.assignment_key, 
                                                  id=student.rose_username,
                                                  score=grade_entry.score,
                                                  student_key=grade_entry.student_key,
-                                                 assignment_key=grade_entry.assignment_key)
+                                                 assignment_key=grade_entry.assignment_key) 
         grade_entry_with_parent.put()
         return grade_entry_with_parent
-    
-    @Assignment.method(user_required="True",
-                       request_fields=("entityKey",),
-                       name="assignment.delete",
-                       path="assignment/delete/{entityKey}", 
-                       http_method="DELETE")
+
+
+    @Assignment.method(user_required=True, request_fields=("entityKey",), 
+                       name="assignment.delete", path="assignment/delete/{entityKey}", http_method="DELETE")
     def assignment_delete(self, assignment):
-        """ delete an assignment with the given key, plus all its associated grade entries """
+        """ Delete the assignment with the given key, plus all its associated grade entries""" 
         if not assignment.from_datastore:
             raise endpoints.NotFoundException("No assignment with this key exists")
         children = GradeEntry.query(ancestor=assignment.key)
@@ -101,16 +81,14 @@ class GradeRecorderApi(protorpc.remote.Service):
         assignment.key.delete()
         return Assignment(name="deleted")
 
-    @GradeEntry.method(user_required="True",
-                       request_fields=("entityKey",),
-                       name="gradeentry.delete",
-                       path="gradeentry/delete/{entityKey}", 
-                       http_method="DELETE")
-    def gradeentry_delete(self, gradeentry):
-        """ delete an gradeentry with the given key """
-        if not gradeentry.from_datastore:
-            raise endpoints.NotFoundException("No gradeentry with this key exists")
-        gradeentry.key.delete()
-        return GradeEntry(score=1)
     
-app = endpoints.api_server([GradeRecorderApi], restricted=False)
+    @GradeEntry.method(user_required=True, request_fields=("entityKey",), 
+                       name="gradeentry.delete", path="gradeentry/delete/{entityKey}", http_method="DELETE")
+    def gradeentry_delete(self, grade_entry):
+        """ Delete the grade entry with the given key""" 
+        if not grade_entry.from_datastore:
+            raise endpoints.NotFoundException("No grade entry with this key exists")
+        grade_entry.key.delete()
+        return GradeEntry(score=-1)
+
+app = endpoints.api_server([GradeRecorderApi], restricted = False)
--- models.py
+++ models.py
@@ -1,23 +1,22 @@
-from endpoints_proto_datastore.ndb.model import EndpointsModel
 from google.appengine.ext import ndb
+from endpoints_proto_datastore.ndb.model import EndpointsModel
 
 class Student(EndpointsModel):
     """ Student. """
-    _message_fields_schema = ("entityKey","first_name","last_name","rose_username","team")
+    _message_fields_schema = ("entityKey", "first_name", "last_name", "rose_username", "team")
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
     rose_username = ndb.StringProperty()
     team = ndb.StringProperty()
 
-
-class Assignment(ndb.Model):
+class Assignment(EndpointsModel):
     """ Assignment. """
-    _message_fields_schema = ("entityKey","name")
+    _message_fields_schema = ("entityKey", "name")
     name = ndb.StringProperty()
 
-class GradeEntry(ndb.Model):
+class GradeEntry(EndpointsModel):
     """ Score for a student on an assignment. """
-    _message_fields_schema = ("entityKey","score","student_key","assignment_key")
+    _message_fields_schema = ("entityKey", "score", "student_key", "assignment_key")
     score = ndb.IntegerProperty()
     student_key = ndb.KeyProperty(kind=Student)
     assignment_key = ndb.KeyProperty(kind=Assignment)
