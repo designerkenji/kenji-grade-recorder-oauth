b96b5b27d7bb11bd3321f3f5bb4ea96736c9c9ea
Kenji <designer.kenji@gmail.com>
2016-05-07 00:12:49 -0400

    debug

--- api.py
+++ api.py
@@ -18,34 +18,46 @@ IOS_CLIENT_ID=""
                version="v1", 
                description="Grade Recorder API", 
                audiences=[WEB_CLIENT_ID],
+               hostname="kenji-grade-recorder-oathu.appspot.com",
                allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID, WEB_CLIENT_ID,ANDROID_CLIENT_ID,IOS_CLIENT_ID])
 class GradeRecorderApi(protorpc.remote.Service):
     pass
 
-    @Student.query_method(user_required="True", query_fields=("limit","order","pageToken"),
-                             name="student.list", path="student/list", http_method="GET")
+    @Student.query_method(user_required="True", 
+                          query_fields=("limit", "pageToken"),
+                          name="student.list", 
+                          path="student/list", 
+                          http_method="GET")
     def student_list(self, query):
         """ list all the students for a given user """
         user = endpoints.get_current_user()
         students = Student.query(ancestor=main.get_parent_key(user)).order(Student.rose_username)
         return students
 
-    @Assignment.query_method(user_required="True", query_fields=("limit","pageToken"),
-                             name="assignment.list", path="assignment/list", http_method="GET")
+    @Assignment.query_method(user_required="True", 
+                             query_fields=("limit","pageToken"),
+                             name="assignment.list", 
+                             path="assignment/list", 
+                             http_method="GET")
     def assignment_list(self, query):
         """ list all the assignments owned by a given user """
         user = endpoints.get_current_user()
         assignments = Assignment.query(ancestor=main.get_parent_key(user)).order(Assignment.name)
         return assignments
 
-    @GradeEntry.query_method(user_required="True", query_fields=("limit","order","pageToken","assignment_key"),
-                             name="gradeentry.list", path="gradeentry/list/{assignment_key}", http_method="GET")
+    @GradeEntry.query_method(user_required="True", 
+                             query_fields=("limit","order","pageToken","assignment_key"),
+                             name="gradeentry.list", 
+                             path="gradeentry/list/{assignment_key}", 
+                             http_method="GET")
     def gradeentry_list(self, query):
         """ list all the grade entries for a given assignment """
         return query
     
     @Assignment.method(user_required="True", 
-                             name="assignment.insert", path="assignment/insert", http_method="POST")
+                       name="assignment.insert", 
+                       path="assignment/insert", 
+                       http_method="POST")
     def assignment_insert(self, assignment):
         """ add or update an assignment owned by the given user """
         if assignment.form_datastore:
@@ -57,7 +69,9 @@ class GradeRecorderApi(protorpc.remote.Service):
         return assignment_with_parent
 
     @GradeEntry.method(user_required="True", 
-                             name="gradeentry.insert", path="gradeentry/insert", http_method="POST")
+                       name="gradeentry.insert", 
+                       path="gradeentry/insert", 
+                       http_method="POST")
     def gradeentry_insert(self, grade_entry):
         """ add or update an grade entry for an assignment """
         if grade_entry.form_datastore:
@@ -99,4 +113,4 @@ class GradeRecorderApi(protorpc.remote.Service):
         gradeentry.key.delete()
         return GradeEntry(score=1)
     
-app = endpoints.api_server(GradeRecorderApi, restricted=False)
+app = endpoints.api_server([GradeRecorderApi], restricted=False)
